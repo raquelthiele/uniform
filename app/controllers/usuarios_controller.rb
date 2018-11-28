70cae3f103c425e9018152dacb43e7de395fcbd9
Raquel Thiele <raquelthiele@hotmail.com>
2018-11-28 17:36:29 -0200

    :rocket: Cria cadastro de usuario

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -6,4 +6,30 @@ class UsuariosController < ApplicationController
   def new
     @usuario = Usuario.new
   end
+
+  def create
+    @usuario = Usuario.new(usuario_params)
+    if @usuario.save
+      flash[:success] = "Obrigada por se cadastrar no Uniform!"
+      redirect_to @usuario
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def usuario_params
+      params.require(:usuario).permit(
+        :cpf,
+        :nome,
+        :email,
+        :celular,
+        :residencial,
+        :tipo,
+        :matricula,
+        :siape,
+        :password,
+        :password_confirmation
+      )
+    end
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,15 +1,20 @@
 class Usuario < ApplicationRecord
 
-  attr_accessor :type, :number
+  attr_accessor :tipo, :matricula, :siape
 
   before_save { self.email = email.downcase }
 
-  validates :cpf, presence: true, uniqueness: true, length: { is: 11 }, numericality: { only_integer: true }
+  validates :cpf, presence: true, uniqueness: true, length: { is: 11 },
+    numericality: { only_integer: true }
   validates :nome, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates :celular, presence: true, length: { is: 11 }, numericality: { only_integer: true }
-  validates :residencial, allow_blank: true, length: { is: 11 }, numericality: { only_integer: true }
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX }
+  validates :celular, presence: true, length: { is: 11 },
+    numericality: { only_integer: true }
+  validates :residencial, allow_blank: true, length: { is: 11 },
+    numericality: { only_integer: true }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  validates :tipo, presence: true
 end
